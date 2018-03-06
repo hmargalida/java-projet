7e5395bf336b603decd9f31e39ce3e16cdf46f57
HMarga <h.margalida@gmail.com>
2018-03-06 09:31:56 +0100

    modif - ajout formatage du statut et couleur

--- projetJava/src/Modele/Statut.java
+++ projetJava/src/Modele/Statut.java
@@ -5,13 +5,31 @@
  */
 package Modele;
 
+import java.awt.Color;
+
 /**
  *
  * @author heloise
  */
 public enum Statut {
-    en_preparation,
-    plannifie,
-    en_cours,
-    terminee;
+    en_preparation("En préparation",Color.LIGHT_GRAY),
+    plannifie("Plannifié",Color.ORANGE),
+    en_cours("En cours",Color.CYAN),
+    terminee("Terminée", Color.GREEN);
+    
+    private String name;
+    private Color color;
+    
+    Statut(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+    
+    public String toString() {
+        return name;
+    }
+    
+    public Color getCouleur() {
+        return this.color;
+    }
 }
