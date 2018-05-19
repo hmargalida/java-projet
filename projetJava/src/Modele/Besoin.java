3e1e159a504864ce835ad94c48c8d825a21c1ead
HMarga <h.margalida@gmail.com>
2018-05-19 15:07:37 +0200

    rollback to commit d7c0f61

--- projetJava/src/Modele/Besoin.java
+++ projetJava/src/Modele/Besoin.java
@@ -13,90 +13,65 @@ import java.util.Map;
  * @author heloise
  */
 public class Besoin {
-
+    
     private int nbPersNecessaire;
     private Map<Competence, Integer> nbPersComp;
-
+    
     /**
      * Constructeur d'un Besoin
-     *
      * @param nbPersNecessaire nb de personne nécessaire sur la mission
      */
     public Besoin(int nbPersNecessaire) {
         this.nbPersNecessaire = nbPersNecessaire;
         nbPersComp = new HashMap<>();
     }
-
+    
     /**
-     * Ajout d'une compétence au besoin de la mission et du nombre d'employés
-     * nécessaire
-     *
+     * Ajout d'une compétence au besoin de la mission et du nombre d'employés nécessaire 
      * @param c Compétence
      * @param nbPers Nombre d'employé nécessaire pour cette compétence
-     * @throws NbEmployeDepasseException si le quotat pour cette compétence est
-     * dépassé
+     * @throws NbEmployeDepasseException si le quotat pour cette compétence est dépassé
      */
     public void besoinParCompetence(Competence c, int nbPers) throws NbEmployeDepasseException {
-        int nbAct = 0;;
-
-        for (Competence comp : this.nbPersComp.keySet()) {
+        int nbAct=0;
+        for(Competence comp : this.nbPersComp.keySet()) {
             nbAct += this.nbPersComp.get(comp);
         }
-
-        if (this.nbPersComp.containsKey(c)) {
-            // Si diminution
-            if (nbPers < this.nbPersComp.get(c)) {
-                nbAct = nbAct - (this.nbPersComp.get(c) - nbPers);
-            }
-            // Si augmentation
-            if (nbPers > this.nbPersComp.get(c)) {
-                nbAct = nbAct + nbPers - this.nbPersComp.get(c);
-            }
-        }
-
-        if (nbAct <= nbPersNecessaire) {
+        if((nbPers+nbAct)<=nbPersNecessaire) {
             this.nbPersComp.put(c, nbPers);
-        } else {
+        }
+        else {
             throw new NbEmployeDepasseException();
         }
     }
-
-    public void removebesoinParCompetence(Competence c) {
-        this.nbPersComp.remove(c);
-    }
-
+    
     /**
-     * Retourne le nombre d'employé demandé pour cette compétence
-     *
+     * Retourne le nombre d'employé demandé pour cette compétence 
      * @param c Compétence
-     * @return int Nombre d'employé
+     * @return int Nombre d'employé 
      */
     public int getNbPersComp(Competence c) {
         return this.nbPersComp.get(c);
     }
-
+    
     /**
      * Retourne le nombre d'employé total pour la mission
-     *
      * @return int Nombre d'employé total
      */
     public int getNbPersNecessaire() {
         return this.nbPersNecessaire;
     }
-
+    
     /**
-     * setteur du nombre d'employé pour la mission
-     *
+     * setteur du nombre d'employé pour la mission 
      * @param nbPers Nombre d'employé total
      */
     public void setNbPersNecessaire(int nbPers) {
         this.nbPersNecessaire = nbPers;
     }
-
+    
     /**
-     * Retourne la map contenant pour chaque compétence, le nombre d'employé a
-     * affecter
-     *
+     * Retourne la map contenant pour chaque compétence, le nombre d'employé a affecter 
      * @return Map Map des besoins
      */
     public Map<Competence, Integer> getMapBesoins() {
